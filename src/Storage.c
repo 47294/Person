c51b3267589d2d049c5281137f52e5c181bf52d5
Artem <45859415+47294@users.noreply.github.com>
2021-11-20 15:00:09 +0300

    Update Storage.c

--- src/Storage.c
+++ src/Storage.c
@@ -13,34 +13,35 @@ void AddPerson(const char* name, int age, const char* city)
 	strcpy(Persons[Size].Name, name);
 	Persons[Size].Age = age;
 	strcpy(Persons[Size].City, city);
-
+	
 	++Size;
 }
 
 void ShowAll()
 {
 	printf("+------------------------------|---|--------------------+\n");
-	printf("|%30s|%3s|%20s|\n","Name","Age","City");
+	printf("|%30s|%3s|%20s|\n", "Name", "Age", "City");
 	printf("|------------------------------|---|--------------------|\n");
-
+	
 	for(int i = 0; i < Size; ++i)
 	{
 		printf("|%30s|%3d|%20s|\n", Persons[i].Name, Persons[i].Age, Persons[i].City);
-		printf("|-----------------------------|---|--------------------|\n");
+		printf("|------------------------------|---|--------------------|\n");
 	}
 }
+
 void SearchByName(const char* name)
 {
-	printf("+-----------------------------|---|------------------|\n");
-	printf("|%30s|%3s|%20s|\n","Name","Age","City");
-	printf("|-----------------------------|---|------------------|\n");
-
+	printf("+------------------------------|---|--------------------+\n");
+	printf("|%30s|%3s|%20s|\n", "Name", "Age", "City");
+	printf("|------------------------------|---|--------------------|\n");
+	
 	for(int i = 0; i < Size; ++i)
 	{
-		if(strstr(Persons[i].Name, name)!=0)
+		if(strstr(Persons[i].Name, name) != 0)
 		{
-			printf("|%30s|%3d|%20s|\n",Persons[i].Name, Persons[i].Age, Persons[i].City);
-			printf("|-----------------------------|---|-------------------|\n");
+			printf("|%30s|%3d|%20s|\n", Persons[i].Name, Persons[i].Age, Persons[i].City);
+			printf("|------------------------------|---|--------------------|\n");
 		}
 	}
 }
